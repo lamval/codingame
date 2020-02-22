9fd88414742f78b733178341efed73f47d65d0bb
Lamouroux Valentin <lamval2002@gmail.com>
2020-02-22 16:40:47 +0100

    add initial code for the-descent

---
+++ The Descent/index.js
@@ -0,0 +1,21 @@
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+
+// game loop
+while (true) {
+    for (let i = 0; i < 8; i++) {
+        const mountainH = parseInt(readline()); // represents the height of one mountain.
+    }
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('4');     // The index of the mountain to fire on.
+
+}
